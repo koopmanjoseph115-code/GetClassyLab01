ee12c0e731a62e0b714fc7749518a4c64d143011
koopmanjoseph115-code <koopmanjoseph115@gmail.com>
2026-09-04 14:23:15 -0400

    Part 1:Person

--- test/SafeInputObjTest.java
+++ test/SafeInputObjTest.java
@@ -21,4 +21,60 @@ public class SafeInputObjTest {
         a passive Junit test suite for it.
          */
     }
+
+    @Test
+    void getRangedInt()
+    {
+        /*because all of the methods are
+        interactive it is not clear how to implement
+        a passive Junit test suite for it.
+         */
+    }
+
+    @Test
+    void getInt()
+    {
+        /*because all of the methods are
+        interactive it is not clear how to implement
+        a passive Junit test suite for it.
+         */
+    }
+
+    @Test
+    void getRangedDouble()
+    {
+        /*because all of the methods are
+        interactive it is not clear how to implement
+        a passive Junit test suite for it.
+         */
+    }
+
+    @Test
+    void getDouble()
+    {
+        /*because all of the methods are
+        interactive it is not clear how to implement
+        a passive Junit test suite for it.
+         */
+    }
+
+    @Test
+    void getYNConfirm()
+    {
+        /*because all of the methods are
+        interactive it is not clear how to implement
+        a passive Junit test suite for it.
+         */
+    }
+
+    @Test
+    void getRegExString()
+    {
+        /*because all of the methods are
+        interactive it is not clear how to implement
+        a passive Junit test suite for it.
+         */
+    }
+
+
 }
